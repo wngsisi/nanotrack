30e680b5a8fe9a662bac5314b42bcaa59de64eac
Shien Wang <63040619+wngsisi@users.noreply.github.com>
2025-03-29 13:36:50 +0800

    Update RTSEyetrackingNANO.py
    使用yolo-pose模型

--- RTSEyetrackingNANO.py
+++ RTSEyetrackingNANO.py
@@ -1,19 +1,22 @@
+
 import threading
 import cv2
-import ObjectDetectionModuleNANO as odm
 import SerialModuleNANO as sm
 import numpy as np
-import base64
 import  sys, os
-import time
+from ultralytics import YOLO
+model = YOLO("yolo11n-pose.pt")
+
+share_mem_file = os.path.join(os.environ['TEMP'], 'camera_frame.mmap')
 sys.path.append(os.getcwd())
 sys.path.append(os.getcwd()+'/../')
-imgstring = 0
+
 frameWidth = 640
 frameHeight = 480
 filp = 2
 rePose = 1
-global conter
+global conter ,nose
+nose=(0,0)
 conter=100
 perrorLR, perrorUD = 0, 0
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -55,11 +58,8 @@ class RTSCapture(cv2.VideoCapture):
         self._reading = False
         if self.frame_receiver.is_alive(): self.frame_receiver.join()
 def findCenter(imgObjects, objects):
-    cx, cy = -1, -1
+
     if len(objects) != 0:
-        x, y, w, h = objects[0]
-        cx = x + w//2
-        cy = y + h//2
         cv2.circle(imgObjects, (cx, cy), 1, (0, 255, 0), cv2.FILLED)
         ih, iw, ic = imgObjects.shape
         cv2.line(imgObjects, (iw//2, cy), (cx, cy), (0, 255, 0), 1)
@@ -68,6 +68,40 @@ def findCenter(imgObjects, objects):
 
 datas = [0, 1]
 datae = [0]
+
+def findnose(img,nose):
+    imgObjects = img.copy()
+    detected = True
+    nose = [-1, -1]
+    results = model.predict(imgObjects, device=0, classes=[0])
+    for result in results:
+        boxes = result.boxes
+
+        if len(boxes)==0:
+            detected = False
+            nose = [-1, -1]
+            return img, nose, detected
+            break
+
+
+        for i in range(len(boxes)):
+            print(len(results))
+            x, y, w, h = map(int, boxes.xywh[i].tolist())
+            x = x - w // 2
+            y = y - h // 2
+            print(x, y, w, h)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        keypoint = result.keypoints
+        for point in range(len(keypoint)):
+            nose = keypoint.xy[point][0]
+            x, y = map(int, nose)
+            intnose=[x,y]
+            if x != 0 or y != 0:
+                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+
+
+    return  img,intnose,detected
 def trackObject(cx, cy, w, h):
     global perrorLR, perrorUD
     kLR = [0.3, 0.3]
@@ -83,19 +117,19 @@ def trackObject(cx, cy, w, h):
         perrorUD = errorUD
         data = [posX, posY]
         sm.sendData(serr, data)
-print("System initialised")
 if __name__ == '__main__':
     rtscap = RTSCapture.create('rtsp://admin:123456@192.168.1.102:554/h264/ch1/sub/av_stream')     # sys.argv[1]
     rtscap.start_read()
     fps = 0.0
+
     while rtscap.isStarted():
         ok, frame = rtscap.read_latest_frame()
         if not ok:
             continue
-
-        frame = cv2.resize(frame, (0, 0), None, 0.5, 0.5)
-        imgObjects, objects = odm.findObjects(frame, faceCascade, 1.08, 10)
-        if len(objects) != 1:
+        frame = cv2.resize(frame, (480,640 ))
+        imgObjects, nose, detected = findnose(frame, nose)
+        #imgObjects, objects = odm.findObjects(frame, faceCascade, 1.08, 10)
+        if not detected:
             if 100 >= conter > 80:
                 conter = conter - 1
             elif 80 >= conter > 0:
@@ -116,22 +150,22 @@ if __name__ == '__main__':
             conter = 100
             pass
 
-        cx, cy, imgObjects = findCenter(imgObjects, objects)
+        #cx, cy, imgObjects = findCenter(imgObjects, objects)
+        cx, cy = nose[0], nose[1]
+        print(type(cx), type(nose))
         h, w, c = imgObjects.shape
-
         cv2.line(imgObjects, (w // 2, 0), (w // 2, h), (255, 0, 255), 1)  
         cv2.line(imgObjects, (0, h // 2), (w, h // 2), (255, 0, 255), 1)
+        print(cx, cy, w, h
+        )
         trackObject(cx, cy, w, h)
-        img = cv2.resize(imgObjects, (0, 0), None, 3, 3)
-        imgstring = base64.b64encode(img)
-        print(type(imgstring))
-        #cv2.imshow("cam", img)
+        img = cv2.resize(imgObjects, (640, 480))
+        cv2.imshow("cam", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            d = bytes.fromhex( 'ff 01 00 00 00 00 01')
+            sm.send(serr,d)
             break
-    print("no video stop.")
-    print("sleeping 20 ")
-    time.sleep(20)
+
     rtscap.stop_read()
     rtscap.release()
-
-
+    cv2.destroyAllWindows()
